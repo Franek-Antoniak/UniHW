77fb018ec4fc43b507c64a5151d40f5ca50b5b01
Franek Antoniak <franekant123@gmail.com>
2022-11-05 18:47:40 +0100

    bugfix

--- PPJ/05.11.2022/uni/src/Exercise8.java
+++ PPJ/05.11.2022/uni/src/Exercise8.java
@@ -1,8 +1,8 @@
 public class Exercise8 {
 	public static void main(String[] args) {
-		double product = 1;
+		double product = 0;
 		for (int i = 0; i < 10; i++) {
-			product *= 1.0 / (1 << i);
+			product += 1.0 / (1 << i);
 			System.out.println(product);
 		}
 	}

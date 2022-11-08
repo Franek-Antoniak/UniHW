44070a44c51f37489248785cae5c458b18f98f35
Franek Antoniak <franekant123@gmail.com>
2022-11-08 20:43:15 +0100

    - refactor

--- 06.11.2022/uni/src/Ex1.java
+++
@@ -1,8 +0,0 @@
-public class Ex1 {
-    public static void main(String[] args) {
-        for (char i = 75; i <= 150; i++) {
-            System.out.printf("%c - %d\n", i, (int)(i));
-        }
-    }
-
-}
